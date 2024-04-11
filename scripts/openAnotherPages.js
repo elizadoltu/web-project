0a3269ae162d476f97a0f0b93739bc99022ef93c
Doltu Teodora - Eliza <66266646+elizadoltu@users.noreply.github.com>
2024-04-11 11:32:48 +0300

    Update openAnotherPages.js

--- scripts/openAnotherPages.js
+++ scripts/openAnotherPages.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const signUpButton = document.getElementById('sign-up-button');
     signUpButton.addEventListener('click', function() {
  
-      window.location.href = "/frontend/pages/signUpPage.html"; // Replace with your HTML file path
+      window.location.href = "/frontend/pages/signUpPage.html"; 
     });
   
   });
