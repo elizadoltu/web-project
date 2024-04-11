2a16c76ae3ede01478d1512e4afa53796ee219af
unknown <elizadoltuofficial@gmail.com>
2024-04-11 19:45:56 +0300

    login and signup pages

---
+++ frontend/styles/sign-up/scripts/navBack.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const arrowBackButton = document.getElementById('arrow-nav-back');
+    arrowBackButton.addEventListener('click', function() {
+        window.location.href = "/index.html";
+    });
+});
--- scripts/openAnotherPages.js
+++ scripts/openAnotherPages.js
@@ -1,9 +1,17 @@
 window.addEventListener('DOMContentLoaded', function() {
     const signUpButton = document.getElementById('sign-up-button');
+    const logInUserButton = document.getElementById('log-in-button');
+    const logInAsAdminButton = document.getElementById('log-in-as-admin-button');
     signUpButton.addEventListener('click', function() {
  
       window.location.href = "/frontend/pages/signUpPage.html"; // Replace with your HTML file path
     });
+    logInUserButton.addEventListener('click', function() {
+      window.location.href = "/frontend/pages/loginUserPage.html";
+    });
+    logInAsAdminButton.addEventListener('click', function() {
+      window.location.href = "/frontend/pages/loginAdminPage.html";
+    })
   
   });
   
